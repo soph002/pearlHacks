a086d5cff2cc5f65060e5badbd236e45ec41beb2
mvolchko <113381324+mvolchko@users.noreply.github.com>
2023-02-11 12:26:18 -0500

    more files

---
+++ main.js
@@ -0,0 +1,9 @@
+var frog = document.getElementById("frog")
+var frog_emoji = "🐸"
+frog.onclick = function(){
+	if(!frog.innerhtml.includes(frog_emoji)){
+		frog.innerhtml = frog.innerhtml + frog_emoji
+	} else {
+		frog.innerhtml = “frog”
+	}
+}
