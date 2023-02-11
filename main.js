11defc1bd007a57416023c34e08ea47ba41fb4d5
mvolchko <113381324+mvolchko@users.noreply.github.com>
2023-02-11 16:53:24 -0500

    calculate original spend function

--- main.js
+++ main.js
@@ -8,3 +8,33 @@ frog.onclick = function(){
 		frog.innerHTML = frog.innerHTML + frog_emoji
 	}
 }
+
+//collecting the user information
+var percentageToSave = document.getElementById("PS").innerHTML
+var avgSalary = document.getElementById("AS").innerHTML
+var calculate = document.getElemenetById("calculate")
+
+//handles the original calculation of PS and AS
+calculate.onclick = function calculateOriginalSpend() {
+	//find a way to double check we are only given numbers
+	if (percentageToSave==null || percentageToSave=="" || avgSalary==null || avgSalary=="" ) {
+		//error message at the top?
+	}
+	else {
+		//double checking I have numbers
+		try {
+			Integer.parseInt(percentageToSave)
+			Integer.parseInt(avgSalary)
+		}
+		catch (NumberFormatException) {
+			return null
+		}
+		//return what the original spend is
+		let answer= (percentageToSave/100) * avgSalary
+		document.getElemenetById("answer").innerHTML=answer
+
+	}
+}
+
+
+
