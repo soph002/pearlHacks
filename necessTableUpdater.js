47faa8117be133032356765e922316300fba8772
sophiehalish <shalish@elon.edu>
2023-02-11 17:54:44 -0500

    new buttons

---
+++ necessTableUpdater.js
@@ -0,0 +1,22 @@
+function addNewRow() {
+    var table = document.getElementById("NecessityTable");
+    var row = table.insertRow(0);
+    var desc = row.insertCell(0);
+    var cost = row.insertCell(1);
+
+    // could be cleaner with for loop. 
+    let promptForDesc = prompt("Please enter the name of subscription:");
+    if (promptForDesc == null || promptForDesc == "") {
+        desc.innerHTML = "invalid";
+    } else {
+        desc.innerHTML = promptForDesc;
+    }
+
+    let promptForCost = prompt("Please enter the name of subscription:");
+    if (promptForCost == null || promptForCost == "") {
+        cost.innerHTML = "invalid";
+    } else {
+        cost.innerHTML = promptForCost;
+    }
+
+}
