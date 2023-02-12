55e0e5b1c451a91f1424de4dc7f9c1fda4132374
mvolchko <113381324+mvolchko@users.noreply.github.com>
2023-02-11 19:01:53 -0500

    im tired

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-var frog = document.getElementById("frog")
-var frog_emoji = "🐸"
-
-frog.onclick = function(){
-	if(frog.innerHTML.includes(frog_emoji)){
-		frog.innerHTML = "frog"
-	} else {
-		frog.innerHTML = frog.innerHTML + frog_emoji
-	}
-}
 
 //collecting the user information
 var percentageToSave = document.getElementById("PS").innerHTML
